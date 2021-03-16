04770987201c1858a52977d9adb9446330d11b64
sachin singh <48687519+sachin3018@users.noreply.github.com>
2021-03-16 10:16:03 +0530

    Update Product.js

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -13,11 +13,6 @@ const Product = ({addInCart,item}) => {
                         className="mb-3 rounded"
                     />
                 </div>
-              <p className="lead  font-weight-normal text-wrap">
-                {item.productName}
-              </p>
-                <p className="btn btn-info rounded  btn-sm px-4">{item.price} $</p>
-                
             </div>
             <p className="btn btn-info rounded  btn-sm px-4 ml-3 mr-3" onClick={() => {
               addInCart(item)
